63458314f70c4e11cc762abccbaa9ae1ed5b8232
Aleksandr Bachurskiy <65046789+alex-bachurskiy@users.noreply.github.com>
2021-05-19 23:15:54 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ let minItem = document.querySelector("#min");
 let secItem = document.querySelector("#sec");
 
 let countDown = () => {
-  let futureDate = new Date("30 Apr 2021");
+  let futureDate = new Date("01 Jan 2022");
   let currentDate = new Date();
   let myDate = futureDate - currentDate;
 
